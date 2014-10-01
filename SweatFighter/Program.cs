892aa0e146995cee1b77ab1f32562b05705251ee
Art2B <arthur.battut@gmail.com>
2014-10-01 12:22:53 +0200

    Init repo and project

---
+++ SweatFighter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SweatFighter
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Welcome in Sweat Fighters ! \nGet ready for the rumble !");
+		}
+	}
+}
